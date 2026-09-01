74fc3fc305003eed924031a6b41d7301252f0f85
nabimo <nabet75m@gmail.com>
2026-09-01 19:31:24 +0330

    fix(database): resolve generated Prisma client import

--- packages/database/src/prisma.ts
+++ packages/database/src/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/client.js";
+import { PrismaClient } from "./generated/client.js";
 
 let client: PrismaClient | undefined;
 
