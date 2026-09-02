b29cf04b84eb1e08098f6cf58a6674d230f8ded0
nabimo <nabet75m@gmail.com>
2026-09-02 08:41:34 +0330

    feat(core): export access token validation service

--- packages/core/src/session/index.ts
+++ packages/core/src/session/index.ts
@@ -3,3 +3,4 @@ export * from "./refresh-token.js";
 export * from "./service.js";
 export * from "./refresh-service.js";
 export * from "./management.js";
+export * from "./access-token-validation.js";
