20367e07e37af0979d922669c64789f1861cce5c
nabimo <nabet75m@gmail.com>
2026-09-03 15:44:52 +0330

    feat(client): export public client API

---
+++ packages/client/src/index.ts
@@ -0,0 +1,13 @@
+export { AuthClient, createAuthClient } from "./client.js";
+export { MemoryTokenStorage } from "./storage.js";
+export { AuthClientError } from "./types.js";
+export type {
+  AuthClientOptions,
+  AuthRequestOptions,
+  AuthTokens,
+  AuthUser,
+  AuthenticationResult,
+  LogoutAllResult,
+  LogoutResult,
+  TokenStorage,
+} from "./types.js";
