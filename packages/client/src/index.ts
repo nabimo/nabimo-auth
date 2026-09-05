5af8249626b3baf3cb0fbe10ca628995337cb6e3
nabimo <nabet75m@gmail.com>
2026-09-05 04:39:12 +0330

    refactor: export refresh coordination API

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,6 +1,8 @@
 export { AuthClient, createAuthClient } from "./client.js";
+export { InMemoryRefreshCoordinator } from "./refresh-coordinator.js";
 export { MemoryTokenStorage } from "./storage.js";
 export { AuthClientError } from "./types.js";
+export type { RefreshCoordinator } from "./refresh-coordinator.js";
 export type {
   AuthClientOptions,
   AuthRequestOptions,
