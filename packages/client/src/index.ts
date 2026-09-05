9cb1203245cb907efc122b1103c6717494ec36a1
nabimo <nabet75m@gmail.com>
2026-09-05 09:20:16 +0330

    feat(client): export refresh transports

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,5 +1,5 @@
 export { AuthClient, createAuthClient } from "./client.js";
-export { InMemoryRefreshCoordinator } from "./refresh-coordinator.js";
+export { BearerRefreshTransport, CookieRefreshTransport, InMemoryRefreshCoordinator } from "./refresh-coordinator.js";
 export { MemoryTokenStorage } from "./storage.js";
 export { AuthClientError } from "./types.js";
 export type {
@@ -15,6 +15,7 @@ export type {
   PasswordResetConfirmResult,
   PasswordResetRequestResult,
   RefreshCoordinator,
+  RefreshTransport,
   TokenStorage,
   TwoFactorSetupResult,
   TwoFactorResult,
