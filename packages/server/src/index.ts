3e7fdbdc5f9cfe27d83ae1c9b9fea45398e1b215
nabimo <nabet75m@gmail.com>
2026-09-06 04:34:24 +0330

    fix: export refresh cookie origin middleware

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,6 +2,6 @@ export { createAuthRouter } from "./routes/auth.js";
 export type { AuthRouteDependencies, RefreshCookieOptions } from "./routes/auth.js";
 export { createIpRateLimitMiddleware, resolveClientIp } from "./ip-rate-limit.js";
 export type { IpRateLimitOptions, IpRateLimitPolicy } from "./ip-rate-limit.js";
-export { createRefreshCookieOriginMiddleware, normalizeAllowedOrigins } from "./refresh-cookie-origin.js";
-export type { RefreshCookieOriginOptions } from "./refresh-cookie-origin.js";
+export { createRefreshCookieOriginMiddleware } from "./refresh-cookie-origin-middleware.js";
+export type { RefreshCookieOriginOptions } from "./refresh-cookie-origin-middleware.js";
 export * from "./api-contract.js";
